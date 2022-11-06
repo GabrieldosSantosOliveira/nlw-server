b980eaa9c24bdb7d6deaec896b8c8582d9211fb2
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-06 17:28:27 -0400

    feat: Add routes guess

---
+++ src/routes/guess.ts
@@ -0,0 +1,12 @@
+import { FastifyInstance } from 'fastify';
+
+import { prisma } from '../lib/prisma';
+
+export const guessRoutes = async (
+  fastify: FastifyInstance
+) => {
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count();
+    return { count };
+  });
+};
