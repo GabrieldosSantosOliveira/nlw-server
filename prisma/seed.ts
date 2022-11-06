832f732ffe322c60625048c3926ef035900ffb5c
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-05 22:12:56 -0400

    feat: Add file seed

---
+++ prisma/seed.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      avatarUrl:
+        'https://github.com/GabrieldosSantosOliveira.png'
+    }
+  });
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Pool 1',
+      code: '123456',
+      ownerId: user.id,
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  });
+  await prisma.game.create({
+    data: {
+      date: '2022-11-06T01:23:27.852Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR'
+    }
+  });
+  await prisma.game.create({
+    data: {
+      date: '2022-11-06T01:23:27.852Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+      guesses: {
+        create: {
+          firstTeamPoints: 4,
+          secondTeamPoints: 2,
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+};
+main();
